adc5f34f25378a09800336452b8b0536d80c6389
kakon4366 <kakonbarman4366@gmail.com>
2022-04-30 21:52:15 +0600

    service path change to inventory

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ async function run() {
 			res.send(services);
 		});
 
-		app.get("/product/:id", async (req, res) => {
+		// get method (single product get)
+		app.get("/inventory/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: ObjectId(id) };
 			const product = await productCollection.findOne(query);
